f08688010a5aa717001fa894952a3164944de615
Daniela_J_H <154357197+DanielaHernande@users.noreply.github.com>
2024-04-02 13:21:58 -0500

    Semana #3

---
+++ Hospital/src/Hospital/src/entity/Medico.java
@@ -0,0 +1,68 @@
+package entity;
+
+public class Medico {
+
+    private int id_medico;
+    private String nombre;
+    private String apellidos;
+    private int id_especialidad;
+
+    // Constructores
+
+    // Vacío
+    public Medico() {
+    }
+
+    // Con parámetros
+    public Medico(int id_medico, String nombre, String apellidos, int id_especialidad) {
+        this.id_medico = id_medico;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.id_especialidad = id_especialidad;
+    }
+
+    // Métodos getters and setters
+    public int getId_medico() {
+        return id_medico;
+    }
+
+    public void setId_medico(int id_medico) {
+        this.id_medico = id_medico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getId_especialidad() {
+        return id_especialidad;
+    }
+
+    public void setId_especialidad(int id_especialidad) {
+        this.id_especialidad = id_especialidad;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "Medico{" +
+                "id_medico=" + id_medico +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", id_especialidad=" + id_especialidad +
+                '}';
+    }
+}
